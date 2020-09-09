6fd12960379ecaf1321420c8a06015f24ff0ec7d
hernandez87v <hernandez87v@hotmail.com>
2020-09-09 10:15:12 -0700

    Add OpenWeather API

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = 'ef465a4fe0e1f90c6289a95dde0abdbc';
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
